dac9eb8685890e32ec3106580ce6283e53c6f513
ritikkumar27 <rkv2771@gmail.com>
2026-03-09 19:23:22 +0530

    added exports in index.ts

--- src/components/portfolio/index.ts
+++ src/components/portfolio/index.ts
@@ -1,1 +1,6 @@
-export { default as Background } from './Background.tsx';
+export { default as Background } from './Background';
+export { default as HeroSection } from './HeroSection';
+export { default as Navigation } from './Navigation';
+export { default as TypewriterRole } from './TypewriterRole';
+
+
